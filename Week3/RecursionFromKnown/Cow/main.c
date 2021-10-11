06f066f4bb1c679deb2ce4934f9e177a3939ac4e
foxwhite25 <vct.xie@gmail.com>
2021-10-11 21:15:05 +0800

    Recursion

--- Week3/RecursionFromKnown/Cow/main.c
+++ Week3/RecursionFromKnown/Cow/main.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 
+int cow(int year) {
+    if (year <= 4) return year;
+    return cow(year - 4) + year;
+}
+
 int main() {
+    setbuf(stdout, NULL);
+    getchar();
     while (1) {
-        setbuf(stdout, NULL);
         int year;
         scanf("%d", &year);
         if (year == 0) break;
-        int cows[year];
-        for (int i = 0; i < year; ++i) {
-            if (i == 0) {
-                cows[i] = 1;
-                continue;
-            }
-            cows[i] = cows[i - 1] + 1;
-            if (i < 3) continue;
-            for (int j = 0; j < (i - 3); ++j) cows[i] += cows[j];
-        }
-        printf("%d\n", cows[year-1]);
+        printf("%d\n", cow(year));
     }
 }
